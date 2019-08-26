9c628cdd3710b043ebacb02478a2ba1421995481
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2019-08-26 16:23:35 +0200

    Remove unused downloads

--- data/download.sh
+++ data/download.sh
@@ -2,15 +2,6 @@
 
 echo Downloading source datasets ...
 
-echo CDIAC
-echo =====
-wget -O nation.1751_2013.csv 'http://cdiac.ornl.gov/ftp/ndp030/CSV-FILES/nation.1751_2013.csv'
-
-echo PRIMAP-hist
-echo ===========
-wget -O PRIMAP-hist_v1.1.zip http://escidoc.gfz-potsdam.de/ir/item/escidoc:2086888/components/component/escidoc:2097890/content
-unzip PRIMAP-hist_v1.1.zip
-
 echo CMIP6 concentration data
 echo ========================
 wget -O mole_fraction_of_carbon_dioxide_in_air_input4MIPs_GHGConcentrations_CMIP_UoM-CMIP-1-1-0_gr3-GMNHSH_000001-201412.csv 'ftp://data.iac.ethz.ch/CMIP6/input4MIPs/UoM/GHGConc/CMIP/mon/atmos/UoM-CMIP-1-1-0/GHGConc/gr3-GMNHSH/v20160701/mole_fraction_of_carbon_dioxide_in_air_input4MIPs_GHGConcentrations_CMIP_UoM-CMIP-1-1-0_gr3-GMNHSH_000001-201412.csv'
